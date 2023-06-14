dbb24ca374213b57f3069081e16749361ef6a203
haydogdu <haldun.aydogdu@beakwise.com>
2023-06-14 16:31:20 +0300

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { App } from 'vue'
 import BWBeakDataTable from './components/V-BeakDataTable.vue'
 import BWFlexPagination from './components/V-FlexPagination.vue'
+import JsonExcel from 'vue-json-excel3'
 
 export default {
   install: (app: App, options) => {
     app.component('BWBeakDataTable', BWBeakDataTable)
     app.component('BWFlexPagination', BWFlexPagination)
+    app.component('JsonExcel', JsonExcel)
   },
 }
