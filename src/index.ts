d5cc60008fc75c49d9a11a462a03eaa68467cd21
haydogdu <haldun.aydogdu@beakwise.com>
2023-06-14 17:15:04 +0300

    include 18n

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { App } from 'vue'
 import BWBeakDataTable from './components/V-BeakDataTable.vue'
 import BWFlexPagination from './components/V-FlexPagination.vue'
 import JsonExcel from 'vue-json-excel3'
+import i18n from './i18n'
 
 export default {
   install: (app: App, options) => {
+    app.use(i18n)
     app.component('BWBeakDataTable', BWBeakDataTable)
     app.component('BWFlexPagination', BWFlexPagination)
     app.component('JsonExcel', JsonExcel)
